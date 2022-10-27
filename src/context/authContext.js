0ce755bc5957b93ff1e509aa3d9b7221706beae2
Aaron Duke <hello@aaronduke.co.uk>
2022-10-27 15:48:11 +0100

    initial user

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -4,7 +4,12 @@ export const AuthContext = createContext()
 
 export const AuthContextProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(
-    JSON.parse(localStorage.getItem("user")) || null
+    JSON.parse(localStorage.getItem("user")) || {
+      id: 1,
+      name: "John Doe",
+      avatar: "https://www.fillmurray.com/50/50",
+      img: "https://www.fillmurray.com/400/200",
+    }
   )
 
   const login = () => {
